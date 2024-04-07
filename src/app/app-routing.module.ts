67eed8fa846cf8479e82dc494ab3fbf30674ded1
Vaisak-P <vaisakp530@gmail.com>
2024-04-07 10:38:56 +0300

    created auth and app manager module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./app-manager/app-manager.module').then(
+        (m) => m.AppManagerModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
